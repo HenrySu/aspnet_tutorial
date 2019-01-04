e476d110fd42133b2746843aaf322807dace2d01
hsu2 <hsu2@slb.com>
2019-01-04 11:17:56 +0800

    [hsu2][enable get ]

---
+++ webapi_spike/Models/TodoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace webapi_spike.Models
+{
+    public class TodoContext:DbContext
+    {
+        public TodoContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
